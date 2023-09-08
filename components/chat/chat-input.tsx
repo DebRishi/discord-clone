3a10158682ad3ab7c5181127d8eef8d7a8639904
Debaditya Mallick <mallick.debaditya@gmail.com>
2023-09-08 21:32:24 +0530

    Added emoji picker and fixed bugs in message input

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -15,6 +15,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useModal } from "@/hooks/use-modal-store";
+import { EmojiPicker } from "@/components/emoji-picker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -35,6 +37,7 @@ export const ChatInput = ({
 }: ChatInputProps) => {
     
     const { onOpen } = useModal();
+    const router = useRouter();
     
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -53,6 +56,9 @@ export const ChatInput = ({
             });
 
             await axios.post(url, values);
+            
+            form.reset();
+            router.refresh();
         } catch (error) {
             console.log(error);
         }
@@ -82,7 +88,9 @@ export const ChatInput = ({
                                         {...field}
                                     />
                                     <div className="absolute top-7 right-8">
-                                        <Smile />
+                                        <EmojiPicker
+                                            onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                                        />
                                     </div>
                                 </div>
                             </FormControl>
